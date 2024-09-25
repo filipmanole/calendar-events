62f2dccae5fe2f3193824deb11c77e553d53fb90
Filip Manole <filip.manole@gmail.com>
2024-09-25 23:12:13 +0300

    overlapping functionality

--- src/data/appointment/createAppointment.ts
+++ src/data/appointment/createAppointment.ts
@@ -1,15 +1,35 @@
 import { AppointmentEntity } from "../../entities/appointment/appointment.entity";
 import documentClient from "../dynamoClientWithConfigs";
 import { Resource } from "sst";
+import { listAppointments } from "./listAppointments";
 
 type CreateAppointmentProps = {
   calendarId: string;
   title: string;
   startAt: string;
   endAt: string;
+  overlap: boolean;
 };
 
 export const createAppointment = async (props: CreateAppointmentProps) => {
+  if (!props.overlap) {
+    const appointments = await listAppointments({
+      calendarId: props.calendarId,
+      date: new Date(props.startAt),
+      type: "DAY",
+    });
+
+    const isOverlapping = appointments.some(
+      (appointment) =>
+        +new Date(appointment.startAt) < +new Date(props.endAt) &&
+        +new Date(appointment.endAt) > +new Date(props.startAt)
+    );
+
+    if (isOverlapping) {
+      throw Error("Overlapping with existing appointment");
+    }
+  }
+
   const appointment = new AppointmentEntity(props);
   try {
     await documentClient.put({
